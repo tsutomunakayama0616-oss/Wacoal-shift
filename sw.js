77919125cd440bfc4f758d3f6e90b1c3b171dd3d
tsutomunakayama0616-oss <tsutomu.nakayama0616@gmail.com>
2026-04-17 11:36:57 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,10 @@
-const CACHE_NAME = 'wacoal-shift-v1';
-// キャッシュするファイルリスト（アイコン画像を追加）
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE_NAME = 'shift-gen-v2';
+const urlsToCache = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
-// インストール時にファイルをキャッシュ
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
 });
 
-// オフライン時はキャッシュから、オンライン時はネットワークから取得
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
