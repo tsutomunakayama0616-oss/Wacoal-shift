02fa60458a9f71eaf6437281df149e0bfa244472
tsutomunakayama0616-oss <tsutomu.nakayama0616@gmail.com>
2026-04-17 12:04:15 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'shift-gen-v2';
+const CACHE_NAME = 'shift-gen-v4';
 const urlsToCache = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
